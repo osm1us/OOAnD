49efd05f067e56fc733cf2c7acec1fc17a9fa742
Sergey Motynga <motynga12345@mail.ru>
2024-11-03 23:00:15 +0600

    test: created tests for class Vector

---
+++ SpaceBattle.Tests/VectorTest.cs
@@ -0,0 +1,42 @@
+﻿namespace SpaceBattle.Tests;
+public class VectorTest
+{
+    [Fact]
+    public void EqualsNotNullTest()
+    {
+        var testVector = new Vector(new List<int> { 1, 2 });
+        Assert.False(Vector.IsNull(testVector));
+    }
+
+    [Fact]
+    public void EqualVectorTest()
+    {
+        var testVector = new Vector(new List<int> { 1, 2 });
+        Assert.True(Vector.IsVector(testVector));
+    }
+
+    [Fact]
+    public void EqualsNotDimensionTest()
+    {
+        var testVector1 = new Vector(new List<int> { 1, 2 });
+        var testVector2 = new Vector(new List<int> { 1, 2, 3 });
+        Assert.False(testVector1.EqualsDimension(testVector2));
+    }
+
+    [Fact]
+    public void EqualsCoordinatesTest()
+    {
+        var testVector1 = new Vector(new List<int> { 1, 2 });
+        _ = new Vector(new List<int> { 1, 2 });
+        Assert.True(testVector1.EqualsCoordinates(testVector1));
+    }
+
+    [Fact]
+    public void AdditionTest()
+    {
+        var testVector1 = new Vector(new List<int> { 1, 2 });
+        var testVector2 = new Vector(new List<int> { 3, 4 });
+        var testResultVector = testVector1 + testVector2;
+        Assert.True(typeof(Vector).IsInstanceOfType(testResultVector));
+    }
+}
