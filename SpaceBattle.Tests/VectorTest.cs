9310813f0ba450fbf55edc714b56a30cbb26adae
Sergey Motynga <motynga12345@mail.ru>
2024-11-04 20:35:33 +0600

    fix: modified tests

--- SpaceBattle.Tests/VectorTest.cs
+++ SpaceBattle.Tests/VectorTest.cs
@@ -4,38 +4,38 @@ public class VectorTest
     [Fact]
     public void EqualsNotNullTest()
     {
-        var testVector = new Vector(new List<int> { 1, 2 });
+        var testVector = new Vector(1, 2);
         Assert.False(Vector.IsNull(testVector));
     }
 
     [Fact]
     public void EqualVectorTest()
     {
-        var testVector = new Vector(new List<int> { 1, 2 });
+        var testVector = new Vector(1, 2);
         Assert.True(Vector.IsVector(testVector));
     }
 
     [Fact]
     public void EqualsNotDimensionTest()
     {
-        var testVector1 = new Vector(new List<int> { 1, 2 });
-        var testVector2 = new Vector(new List<int> { 1, 2, 3 });
+        var testVector1 = new Vector(1, 2);
+        var testVector2 = new Vector(1, 2, 3);
         Assert.False(testVector1.EqualsDimension(testVector2));
     }
 
     [Fact]
     public void EqualsCoordinatesTest()
     {
-        var testVector1 = new Vector(new List<int> { 1, 2 });
-        _ = new Vector(new List<int> { 1, 2 });
-        Assert.True(testVector1.EqualsCoordinates(testVector1));
+        var testVector1 = new Vector(1, 2);
+        var testVector2 = new Vector(1, 2);
+        Assert.True(testVector1.EqualsCoordinates(testVector2));
     }
 
     [Fact]
     public void AdditionTest()
     {
-        var testVector1 = new Vector(new List<int> { 1, 2 });
-        var testVector2 = new Vector(new List<int> { 3, 4 });
+        var testVector1 = new Vector(1, 2);
+        var testVector2 = new Vector(1, 2);
         var testResultVector = testVector1 + testVector2;
         Assert.True(typeof(Vector).IsInstanceOfType(testResultVector));
     }
