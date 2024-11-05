3cc460e16d0832cc3f385fccc7d834a5cf45b664
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-11-05 19:29:15 +0600

    feat: rotate

---
+++ SpaceBattle.Lib/2_Rotating/Angle.cs
@@ -0,0 +1,15 @@
+﻿namespace SpaceBattle.Lib;
+public class Angle
+{
+    public int deg;
+    public Angle(int deg)
+    {
+        this.deg = deg;
+    }
+    public static Angle operator +(Angle ang1, Angle ang2)
+    {
+        var ResultAngle = new Angle(0);
+        ResultAngle.deg = (ang1.deg + ang2.deg) % 360;
+        return ResultAngle;
+    }
+}
---
+++ SpaceBattle.Lib/2_Rotating/Rotate.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IRotating
+{
+    Angle AnglePos { get; set; } //45
+    Angle RotateVelocity { get; } //90
+} // AnglePos => 135
+
+public class RotateCommand : ICommand
+{
+    private readonly IRotating obj;
+    public RotateCommand(IRotating obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.AnglePos = obj.AnglePos + obj.RotateVelocity;
+    }
+}
