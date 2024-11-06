b935687a8970d17eb58840d156f1d12a2958d777
sergeyfelde <felde990@mail.ru>
2024-11-06 19:10:57 +0600

    feat: Created Angle, Rotate and Tests
    fix: Created Angle, Rotate and Tests
    
    fix: Created Angle, Rotate and Tests
    
    feat: Created Angle, Rotate and Tests

---
+++ SpaceBattle.Tests/Rotation.Tests/AngleTest.cs
@@ -0,0 +1,67 @@
+﻿using SpaceBattle.Lib;
+namespace Spacebattle.Tests;
+
+public class AngleTests
+{
+    [Fact]
+    public void AngleSumTest()
+    {
+        var angle1 = new Angle(45);
+        var angle2 = new Angle(55);
+        Assert.Equal(new Angle(100), angle1 + angle2);
+    }
+    [Fact]
+    public void RemainsTest()
+    {
+        var angle1 = new Angle(848);
+        var angle2 = new Angle(4054);
+        Assert.Equal(new Angle(222), angle1 + angle2);
+    }
+    [Fact]
+    public void GetHashCode_ShouldReturnSameHash_ForEqualAngles()
+    {
+        var angle1 = new Angle(90);
+        var angle2 = new Angle(90);
+        Assert.Equal(angle1.GetHashCode(), angle2.GetHashCode());
+    }
+    [Fact]
+    public void GetHashCode_ShouldReturnDifferentHash_ForDifferentAngles()
+    {
+        var angle1 = new Angle(90);
+        var angle2 = new Angle(180);
+        Assert.NotEqual(angle1.GetHashCode(), angle2.GetHashCode());
+    }
+    [Fact]
+    public void GetHashCode_ShouldReturnSameHash_ForAnglesWithSameValueAfterNormalization()
+    {
+        var angle1 = new Angle(450); // 450 % 360 = 90
+        var angle2 = new Angle(90);
+        Assert.Equal(angle1.GetHashCode(), angle2.GetHashCode());
+    }
+    [Fact]
+    public void Equals_ShouldReturnTrue_ForSameAngle()
+    {
+        var angle1 = new Angle(90);
+        var angle2 = new Angle(90);
+        Assert.True(angle1.Equals(angle2));
+    }
+    [Fact]
+    public void Equals_ShouldReturnFalse_ForDifferentAngles()
+    {
+        var angle1 = new Angle(90);
+        var angle2 = new Angle(180);
+        Assert.False(angle1.Equals(angle2));
+    }
+    [Fact]
+    public void Equals_ShouldReturnFalse_ForNullObject()
+    {
+        var angle = new Angle(90);
+        Assert.False(angle.Equals(null));
+    }
+    [Fact]
+    public void Equals_ShouldReturnFalse_ForNonAngleObject()
+    {
+        var angle = new Angle(90);
+        Assert.False(angle.Equals("Не Angle"));
+    }
+}
---
+++ SpaceBattle.Tests/Rotation.Tests/RotatingTest.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+namespace SpaceBattle.Tests;
+
+public class RotateCommandTestTest
+{
+    [Fact]
+    public void Rotating45DegreesWithVelocity90Test()
+    {
+        var Rotating = new Mock<IRotating>();
+        Rotating.SetupGet(x => x.AnglePos).Returns(new Angle(45));
+        Rotating.SetupGet(x => x.RotateVelocity).Returns(new Angle(90));
+        var command = new RotateCommand(Rotating.Object);
+        command.Execute();
+        Rotating.VerifySet(x => x.AnglePos = new Angle(135));
+    }
+
+    [Fact]
+    public void NoAnglePosTest()
+    {
+        var Rotating = new Mock<IRotating>();
+        Rotating.SetupGet(x => x.AnglePos).Throws<Exception>();
+        Rotating.SetupGet(x => x.RotateVelocity).Returns(new Angle(90));
+        var command = new RotateCommand(Rotating.Object);
+        Assert.Throws<Exception>(() => command.Execute());
+    }
+
+    [Fact]
+    public void NoRotateVelocityTest()
+    {
+        var Rotating = new Mock<IRotating>();
+        Rotating.SetupGet(x => x.AnglePos).Returns(new Angle(90));
+        Rotating.SetupGet(x => x.RotateVelocity).Throws<Exception>();
+        var command = new RotateCommand(Rotating.Object);
+        Assert.Throws<Exception>(() => command.Execute());
+    }
+
+    [Fact]
+    public void CantChangeAnglePosTest()
+    {
+        var Rotating = new Mock<IRotating>();
+        Rotating.SetupGet(x => x.AnglePos).Returns(new Angle(0));
+        Rotating.SetupSet(x => x.AnglePos = It.IsAny<Angle>()).Throws<Exception>();
+        Rotating.SetupGet(x => x.RotateVelocity).Returns(new Angle(848));
+        var command = new RotateCommand(Rotating.Object);
+        Assert.Throws<Exception>(() => command.Execute());
+    }
+}
