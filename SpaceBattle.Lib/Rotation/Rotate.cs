dff9feb46f49dd0f67a12fecee209fd138e82ec2
sergeyfelde <felde990@mail.ru>
2024-11-06 19:10:57 +0600

    feat: Created Angle, Rotate and Tests
    fix: Created Angle, Rotate and Tests

---
+++ SpaceBattle.Lib/Rotation/Rotate.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IRotating
+{
+    Angle AnglePos { get; set; }
+    Angle RotateVelocity { get; }
+}
+
+public class RotateCommand : ICommand
+{
+    private readonly IRotating obj;
+    public RotateCommand(IRotating obj)
+    {
+        this.obj = obj;
+    }
+    public void Execute()
+    {
+        obj.AnglePos = obj.AnglePos + obj.RotateVelocity;
+    }
+}
