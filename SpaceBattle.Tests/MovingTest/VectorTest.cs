d292e096b00158e7de23fa100d072b1b027efc2d
Sergey Motynga <motynga12345@mail.ru>
2024-11-24 22:30:34 +0600

    feat: Vector and VectorTests

---
+++ SpaceBattle.Tests/MovingTest/VectorTest.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceBattle.Tests;
+public class VectorTest
+{
+    [Fact]
+    public void NotEqualsCoordinatesTest()
+    {
+        var testVector1 = new Vector(1, 2);
+        var testVector2 = new Vector(1, 2, 3);
+        Assert.False(testVector1.Equals(testVector2));
+    }
+
+    [Fact]
+    public void AdditionTest()
+    {
+        var testVector1 = new Vector(1, 2);
+        var testVector2 = new Vector(1, 2);
+        var testResultVector = testVector1 + testVector2;
+        Assert.True(typeof(Vector).IsInstanceOfType(testResultVector));
+    }
+}
