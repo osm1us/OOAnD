c1306ee5588e0be17f6ee8a40bca5362cc2045cd
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-11-27 20:34:32 +0600

    feat: stoprotate

---
+++ SpaceBattle.Lib/Rotation/StopRotate.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceBattle.Lib;
+
+public class StopRotateCommand : ICommand
+{
+    private readonly IDictionary<string, object> _gameObject;
+
+    public StopRotateCommand(IDictionary<string, object> gameObject)
+    {
+        _gameObject = gameObject;
+    }
+
+    public void Execute()
+    {
+        if (!_gameObject.ContainsKey("repeatableRotate"))
+        {
+            throw new InvalidOperationException("Поворот не был запущен");
+        }
+
+        var injectable = (Injectable)_gameObject["repeatableRotate"];
+        injectable.Inject(new EmptyCommand());
+    }
+}
---
+++ SpaceBattle.Tests/Rotation.Tests/StopRotateTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class StopRotateTest
+{
+    [Fact]
+    public void StopRotateSuccessTest()
+    {
+        var gameObject = new Dictionary<string, object>();
+        var injectable = new Mock<Injectable>();
+        gameObject["repeatableRotate"] = injectable.Object;
+        var stopRotate = new StopRotateCommand(gameObject);
+
+        stopRotate.Execute();
+
+        injectable.Verify(x => x.Inject(It.IsAny<EmptyCommand>()));
+    }
+
+    [Fact]
+    public void StopRotateWithoutStartTest()
+    {
+        var gameObject = new Dictionary<string, object>();
+        var stopRotate = new StopRotateCommand(gameObject);
+
+        Assert.Throws<InvalidOperationException>(() => stopRotate.Execute());
+    }
+}
