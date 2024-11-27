7da5daac58c4d57b64318eeb58d3495864698ab6
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-11-27 20:46:14 +0600

    feat: startrotate

---
+++ SpaceBattle.Lib/Rotation/StartRotate.cs
@@ -0,0 +1,23 @@
+﻿namespace SpaceBattle.Lib;
+
+public class StartRotateCommand : ICommand
+{
+    private readonly Queue<ICommand> q = new Queue<ICommand>();
+    private readonly IDictionary<string, object> _gameObject;
+
+    public StartRotateCommand(IDictionary<string, object> gameObject)
+    {
+        _gameObject = gameObject;
+    }
+
+    public void Execute()
+    {
+        var rotateCommand = new RotateCommand(new RotatingAdapter(_gameObject));
+        var injectable = new InjectableCommand();
+        var repeat = new RepeatCommand(injectable, q);
+        var repeatableRotate = new SimpleMacroCommand(rotateCommand, repeat);
+        injectable.Inject(repeatableRotate);
+        _gameObject["repeatableRotate"] = injectable;
+        q.Enqueue(repeatableRotate);
+    }
+}
---
+++ SpaceBattle.Tests/Rotation.Tests/StartRotateTest.cs
@@ -0,0 +1,36 @@
+﻿using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class StartRotateTest
+{
+    [Fact]
+    public void StartRotateSuccessTest()
+    {
+        var gameObject = new Dictionary<string, object>
+        {
+            ["AnglePos"] = new Angle(45),
+            ["RotateVelocity"] = new Angle(90)
+        };
+        var startRotate = new StartRotateCommand(gameObject);
+        startRotate.Execute();
+        Assert.True(gameObject.ContainsKey("repeatableRotate"));
+    }
+
+    [Fact]
+    public void RotationIntegrationTest()
+    {
+        var gameObject = new Dictionary<string, object>
+        {
+            ["AnglePos"] = new Angle(0),
+            ["RotateVelocity"] = new Angle(10)
+        };
+        var startRotate = new StartRotateCommand(gameObject);
+
+        startRotate.Execute();
+        var cmd = (ICommand)gameObject["repeatableRotate"];
+
+        cmd.Execute();
+        Assert.Equal(gameObject["AnglePos"], new Angle(10));
+    }
+}
