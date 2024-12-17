8074c8b6f6eda0bcebe0ea9a37287d0b4706163e
Sergey Motynga <motynga12345@mail.ru>
2024-12-17 23:44:15 +0600

    feat: created Move.cs and tests
    sq move

---
+++ SpaceBattle.Lib/Motion/Move.cs
@@ -0,0 +1,21 @@
+﻿namespace SpaceBattle.Lib;
+
+public interface IMoving
+{
+    Vector Position { get; set; }
+    Vector Velocity { get; }
+}
+public class MoveCommand : ICommand
+{
+    private readonly IMoving obj;
+
+    public MoveCommand(IMoving obj)
+    {
+        this.obj = obj;
+    }
+
+    public void Execute()
+    {
+        obj.Position += obj.Velocity;
+    }
+}
---
+++ SpaceBattle.Tests/MotionTests/MovingTest.cs
@@ -0,0 +1,50 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests
+{
+    public class MoveCommandTests
+    {
+        [Fact]
+        public void UpdatesPositionCorrectlyTest()
+        {
+            var movingObj = new Mock<IMoving>();
+            movingObj.SetupGet(m => m.Position).Returns(new Vector(12, 5));
+            movingObj.SetupGet(m => m.Velocity).Returns(new Vector(-4, 1));
+            var cmd = new MoveCommand(movingObj.Object);
+            cmd.Execute();
+            movingObj.VerifySet(x => x.Position = It.Is<Vector>(x => x.Equals(new Vector(8, 6))));
+        }
+
+        [Fact]
+        public void CannotReadPositionTest()
+        {
+            var movingObj = new Mock<IMoving>();
+            movingObj.SetupGet(m => m.Position).Throws<Exception>();
+            movingObj.SetupGet(m => m.Velocity).Returns(new Vector(-7, 3));
+            var cmd = new MoveCommand(movingObj.Object);
+            Assert.Throws<Exception>(() => cmd.Execute());
+        }
+
+        [Fact]
+        public void CannotReadVelocityTest()
+        {
+            var movingObj = new Mock<IMoving>();
+            movingObj.SetupGet(m => m.Position).Returns(new Vector(12, 5));
+            movingObj.SetupGet(m => m.Velocity).Throws<Exception>();
+            var cmd = new MoveCommand(movingObj.Object);
+            Assert.Throws<Exception>(() => cmd.Execute());
+        }
+
+        [Fact]
+        public void CannotSetPositionTest()
+        {
+            var movingObj = new Mock<IMoving>();
+            movingObj.SetupGet(m => m.Position).Returns(new Vector(12, 5));
+            movingObj.SetupGet(m => m.Velocity).Returns(new Vector(-7, 3));
+            movingObj.SetupSet(m => m.Position = It.IsAny<Vector>()).Throws<Exception>();
+            var cmd = new MoveCommand(movingObj.Object);
+            Assert.Throws<Exception>(() => cmd.Execute());
+        }
+    }
+}
