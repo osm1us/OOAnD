00331cce9bd078367581e0378b1e8e727f1a4021
Sergey Motynga <motynga12345@mail.ru>
2024-12-17 23:43:53 +0600

    feat: Vector.cs and tests
    sq vec

---
+++ SpaceBattle.Lib/Motion/Vector.cs
@@ -0,0 +1,48 @@
+﻿public class Vector
+{
+    private int[] coordinates;
+
+    public Vector(params int[] coordinates)
+    {
+        if (coordinates == null || coordinates.Length == 0)
+        {
+            throw new ArgumentException(nameof(coordinates), "Вектор не может быть пустым.");
+        }
+
+        this.coordinates = coordinates;
+    }
+
+    public static Vector operator +(Vector vector1, Vector vector2)
+    {
+        if (vector1.Dimension != vector2.Dimension)
+        {
+            throw new ArgumentException("Размерности векторов не совпадают.");
+        }
+
+        var resultCoordinates = new Vector(new int[vector1.Dimension]);
+        resultCoordinates.coordinates = vector1.coordinates.Select((с, index) => с + vector2.coordinates[index]).ToArray();
+        return resultCoordinates;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj != null && obj is Vector otherVector && coordinates.SequenceEqual(otherVector.coordinates);
+    }
+
+    public override int GetHashCode()
+    {
+        return coordinates.GetHashCode();
+    }
+
+    public static bool operator ==(Vector? vector1, Vector? vector2)
+    {
+        return !(vector1 is null || vector2 is null) && vector1.Equals(vector2);
+    }
+
+    public static bool operator !=(Vector? vector1, Vector? vector2)
+    {
+        return !(vector1 == vector2);
+    }
+
+    public int Dimension => coordinates.Length;
+}
