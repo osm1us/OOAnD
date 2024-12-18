618f22a72146f34be6951b1945cf3b37e4c20c28
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-12-18 21:08:59 +0600

    feat: Commands.Send

---
+++ SpaceBattle.Lib/Commands/RegisterIoCDependencySendCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencySendCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.Send",
+                (object[] args) => (ICommand)new SendCommand((ICommand)args[0], (ICommandReceiver)args[1])).Execute();
+    }
+}
