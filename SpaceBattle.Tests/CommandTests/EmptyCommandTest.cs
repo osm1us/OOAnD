e708833ce73c4cc0ae3b33a9975206a3687cc905
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-12-18 21:09:36 +0600

    feat: Actions.Stop

---
+++ SpaceBattle.Tests/CommandTests/EmptyCommandTest.cs
@@ -0,0 +1,13 @@
+﻿using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class EmptyCommandTest
+{
+    [Fact]
+    public void EmptyCommand_ExecuteDoesNotThrow()
+    {
+        var cmd = new EmptyCommand();
+        Assert.Null(Record.Exception(() => cmd.Execute()));
+    }
+}
---
+++ SpaceBattle.Tests/CommandTests/RegisterIoCDependencyActionsStopTests.cs
@@ -0,0 +1,26 @@
+﻿namespace SpaceBattle.Tests;
+
+using Hwdtech;
+using Hwdtech.Ioc;
+using SpaceBattle.Lib;
+
+public class RegisterIoCDependencyActionsStopTests
+{
+    public RegisterIoCDependencyActionsStopTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>("Scopes.Current.Set",
+        IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+
+    [Fact]
+    public void RegisterIoCDependencyActionsStop_Should_Register_Stop_Command()
+    {
+        var gameObject = new Dictionary<string, object>();
+
+        new RegisterIoCDependencyActionsStop().Execute();
+        var stopcmd = IoC.Resolve<ICommand>("Actions.Stop", gameObject, "Move");
+
+        Assert.IsType<StopCommand>(stopcmd);
+    }
+}
