a5c5765cc7b86e99082b980b517e3b6083a7b7f4
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-12-18 21:08:46 +0600

    feat: SendCommand

---
+++ SpaceBattle.Tests/CommandTests/SendCommandTests.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using SpaceBattle.Lib;
+
+namespace SpaceBattle.Tests;
+
+public class SendCommandTests
+{
+    [Fact]
+    public void SendCommand_Should_Send_A_Command_To_The_Command_Receiver()
+    {
+        var receiver = new Mock<ICommandReceiver>();
+        var cmd = new Mock<ICommand>();
+        var sendCommand = new SendCommand(cmd.Object, receiver.Object);
+
+        sendCommand.Execute();
+
+        receiver.Verify(x => x.Receive(cmd.Object));
+    }
+    [Fact]
+    public void SendCommand_Should_Throw_When_Receiver_Fails()
+    {
+        var cmd = new Mock<ICommand>();
+        var receiver = new Mock<ICommandReceiver>();
+        receiver.Setup(r => r.Receive(cmd.Object)).Throws<Exception>();
+
+        var sendCommand = new SendCommand(cmd.Object, receiver.Object);
+
+        Assert.Throws<Exception>(() => sendCommand.Execute());
+    }
+}
