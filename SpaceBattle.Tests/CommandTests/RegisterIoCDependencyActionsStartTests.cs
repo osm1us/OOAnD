b8f1ff9fe58437e15d9b5c45be75b1fb2cd2b403
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-12-18 21:09:27 +0600

    feat: Actions.Start

---
+++ SpaceBattle.Tests/CommandTests/RegisterIoCDependencyActionsStartTests.cs
@@ -0,0 +1,28 @@
+﻿namespace SpaceBattle.Tests;
+
+using Hwdtech;
+using Hwdtech.Ioc;
+using SpaceBattle.Lib;
+public class RegisterIoCDependencyActionsStartTests
+{
+    public RegisterIoCDependencyActionsStartTests()
+    {
+        new InitScopeBasedIoCImplementationCommand().Execute();
+        IoC.Resolve<ICommand>("Scopes.Current.Set",
+        IoC.Resolve<object>("Scopes.New", IoC.Resolve<object>("Scopes.Root"))).Execute();
+    }
+
+    [Fact]
+    public void RegisterIoCDependencyActionsStart_Should_Register_Start_Command()
+    {
+        // Arrange
+        var gameObject = new Dictionary<string, object>();
+
+        // Act
+        new RegisterIoCDependencyActionsStart().Execute();
+        var command = IoC.Resolve<ICommand>("Actions.Start", gameObject, "Move");
+
+        // Assert
+        Assert.IsType<StartCommand>(command);
+    }
+}
