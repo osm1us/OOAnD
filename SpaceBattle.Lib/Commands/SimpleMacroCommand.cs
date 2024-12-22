2027c9a7b1f34838ea50480d20c7612d1e5baa5a
Nikolay <kryakayushij@gmail.com>
2024-12-22 15:10:15 +0600

    feat: SimpleMacroCommand

---
+++ SpaceBattle.Lib/Commands/SimpleMacroCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace SpaceBattle.Lib;
+public class SimpleMacroCommand : ICommand
+{
+    public ICommand[] cmds;
+    public SimpleMacroCommand(params ICommand[] cmds)
+    {
+        this.cmds = cmds;
+    }
+    public void Execute()
+    {
+        cmds.All(c =>
+        {
+            c.Execute();
+            return true;
+        });
+    }
+}
