2ca33658132c2e332e599dce9c83138b90e7b1ea
Nikolay <kryakayushij@gmail.com>
2024-12-22 14:56:50 +0600

    feat: IoCMacroCommand

---
+++ SpaceBattle.Lib/Commands/RegisterIoCDependencyMacroCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceBattle.Lib
+{
+    public class RegisterIoCDependencyMacroCommand : ICommand
+    {
+        public void Execute()
+        {
+            IoC.Resolve<ICommand>(
+                "IoC.Register",
+                "Commands.Macro",
+                (object[] args) => (ICommand)new SimpleMacroCommand(args.Select(x => (ICommand)x).ToArray())).Execute();
+        }
+    }
+}
