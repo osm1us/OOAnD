d7f5046dd9338d4c5e677fe7c488d6365786fc8e
sergeyfelde <felde990@mail.ru>
2024-12-23 11:56:05 +0600

    feat: Angle and tests

---
+++ SpaceBattle.Lib/Rotation/Angle.cs
@@ -0,0 +1,34 @@
+﻿public class Angle
+{
+    private int num { get; set; }
+    private int den { get; }
+
+    public Angle(int num, int den)
+    {
+        this.den = den;
+        this.num = ((num % den) + den) % den;
+    }
+
+    public static Angle operator +(Angle ang1, Angle ang2)
+    {
+        return new Angle(ang1.num + ang2.num, ang1.den);
+    }
+
+    public override bool Equals(object? obj) => obj != null && obj is Angle angle && num == angle.num && den == angle.den;
+
+    public override int GetHashCode() => num.GetHashCode();
+
+    public static bool operator ==(Angle ang1, Angle ang2)
+    {
+        return ang1.Equals(ang2);
+    }
+
+    public static bool operator !=(Angle ang1, Angle ang2)
+    {
+        return !(ang1 == ang2);
+    }
+    public static implicit operator double(Angle angle)
+    {
+        return 2 * Math.PI * angle.num / angle.den;
+    }
+}
---
+++ SpaceBattle.Tests/RotationTests/AngleTest.cs
@@ -0,0 +1,101 @@
+﻿namespace Spacebattle.Tests;
+
+public class AngleTests
+{
+    [Fact]
+    public void AngleSumTest()
+    {
+        var angle1 = new Angle(5, 8);
+        var angle2 = new Angle(7, 8);
+        Assert.Equal(new Angle(4, 8), angle1 + angle2);
+    }
+
+    [Fact]
+    public void AngleSum_WithOverflow_Test()
+    {
+        var angle1 = new Angle(7, 8);
+        var angle2 = new Angle(6, 8);
+        Assert.Equal(new Angle(5, 8), angle1 + angle2);
+    }
+
+    [Fact]
+    public void EqualsTest_ShouldReturnTrue()
+    {
+        var angle1 = new Angle(15, 8);
+        var angle2 = new Angle(23, 8);
+        Assert.True(angle1.Equals(angle2));
+    }
+
+    [Fact]
+    public void OperatorEqualsTest_ShouldReturnTrue()
+    {
+        var angle1 = new Angle(15, 8);
+        var angle2 = new Angle(23, 8);
+        Assert.True(angle1 == angle2);
+    }
+
+    [Fact]
+    public void EqualsTest_ShouldReturnFalse()
+    {
+        var angle1 = new Angle(1, 8);
+        var angle2 = new Angle(2, 8);
+        Assert.False(angle1.Equals(angle2));
+    }
+
+    [Fact]
+    public void OperatorNotEqualsTest_ShouldReturnTrue()
+    {
+        var angle1 = new Angle(1, 8);
+        var angle2 = new Angle(2, 8);
+        Assert.True(angle1 != angle2);
+    }
+
+    [Fact]
+    public void GetHashCode_ShouldReturnHashCode()
+    {
+        var angle = new Angle(1, 8);
+        Assert.NotEqual(0, angle.GetHashCode());
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_ForNullObject()
+    {
+        var angle = new Angle(1, 8);
+        Assert.False(angle.Equals(null));
+    }
+
+    [Fact]
+    public void Equals_ShouldReturnFalse_ForNonAngleObject()
+    {
+        var angle = new Angle(1, 8);
+        Assert.False(angle.Equals("Не Angle"));
+    }
+
+    [Fact]
+    public void Sin_ShouldReturnCorrectValue()
+    {
+        var angle = new Angle(2, 8);
+        Assert.Equal(1.0, Math.Sin(angle), precision: 6);
+    }
+
+    [Fact]
+    public void Cos_ShouldReturnCorrectValue()
+    {
+        var angle = new Angle(2, 8);
+        Assert.Equal(0.0, Math.Cos(angle), precision: 6);
+    }
+
+    [Fact]
+    public void Constructor_ShouldNormalizeNegativeAngle()
+    {
+        var angle = new Angle(-1, 8);
+        Assert.Equal(new Angle(7, 8), angle);
+    }
+
+    [Fact]
+    public void Constructor_ShouldNormalizeOverflowAngle()
+    {
+        var angle = new Angle(9, 8);
+        Assert.Equal(new Angle(1, 8), angle);
+    }
+}
