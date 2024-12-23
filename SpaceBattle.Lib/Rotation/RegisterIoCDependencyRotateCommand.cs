fd32dbb53023e23e35739cde3ec464ed5e569929
sergeyfelde <felde990@mail.ru>
2024-12-23 11:57:13 +0600

    feat: RegisterIoCDependencyRotateCommand and tests

---
+++ SpaceBattle.Lib/Rotation/RegisterIoCDependencyRotateCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyRotateCommand : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>(
+            "IoC.Register",
+            "Commands.Rotate",
+            (object[] args) => new RotateCommand(new RotatingAdapter((IDictionary<string, object>)args[0]))
+        ).Execute();
+    }
+}
