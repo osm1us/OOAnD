b3519956a36886a8bf8e7768f12241985b7d59dc
Dmitriy Matveev <channelsmoothie@gmail.com>
2024-12-23 13:49:11 +0600

    feat: Actions.Stop

---
+++ SpaceBattle.Lib/Commands/RegisterIoCDependencyActionsStop.cs
@@ -0,0 +1,19 @@
+﻿namespace SpaceBattle.Lib;
+
+public class RegisterIoCDependencyActionsStop : ICommand
+{
+    public void Execute()
+    {
+        IoC.Resolve<ICommand>(
+            "IoC.Register",
+            "Actions.Stop",
+            (object[] args) =>
+            {
+                var gameObject = (IDictionary<string, object>)args[0];
+                var cmdType = (string)args[1];
+
+                return new StopCommand(gameObject, cmdType);
+            }
+        ).Execute();
+    }
+}
